58534ac895ffab7241316b9528321b9142f2588b
kunpengjiang <1700012912@pku.edu.cn>
2020-11-30 04:48:18 +0000

    Forget to commit global.h

--- src/global.h
+++ src/global.h
@@ -1,6 +1,15 @@
+#ifndef GLOBAL
+#define GLOBAL
 #pragma once
 
 // STL libraries
+#include <cstdint>
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
+
+// Type define
+
+typedef std::int64_t DBU;
+#endif 
