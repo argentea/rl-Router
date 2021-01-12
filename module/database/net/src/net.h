8b6d12f665c8098487f1f9b6c166fb09bccf90dd
kunpengjiang <1700012912@pku.edu.cn>
2021-01-12 10:46:01 +0000

    Add guide topo

---
+++ module/database/net/src/net.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <parser/src/Parser.h>
+namespace db {
+
+class Net{
+public:
+	Net(int i, parser::Net net);
+
+};
+
+
+};
---
+++ module/database/topo/test.cpp
@@ -0,0 +1,6 @@
+#include <unistd.h>
+#include"src/topo.h"
+int main() {
+	std::cout << "topo test\n";
+    return 0;
+}
